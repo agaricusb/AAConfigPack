4297d6ae3c15728f0b1f07c72aa41381dd82462c
Agaricus <agaricusb@yahoo.com>
2013-04-06 21:10:15 -0700

    Prevent server.properties from changing on every server run
    Since Minecraft writes a timestamp, and does not sort the keys..
    make a backup before running the server, then restore it after.
    This way the server.properties will only have our changes, instead
    of irrelevant changes cluttering up git history on each run.

--- go15.sh
+++ go15.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+cp server.properties server.properties.orig
 rm -rf *log*
 #java -Dfml.debugClassLoading=true -Dfml.debugClassLoadingFiner=true -mx2G -jar snapshot.jar
 java -mx2G -jar snapshot.jar
-
+mv server.properties.orig server.properties
