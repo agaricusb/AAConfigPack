c422588b579bdcaf00b5a9fd02c8d945914bbcc0
Agaricus <agaricusb@yahoo.com>
2013-04-07 21:42:10 -0700

    Only allow GuiAPI to load client-side

--- go15.sh
+++ go15.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+chmod 000 coremods/GuiAPI*
 cp server.properties server.properties.orig
 rm -rf *log*
 #java -Dfml.debugClassLoading=true -Dfml.debugClassLoadingFiner=true -mx2G -jar snapshot.jar
 java -mx2G -jar snapshot.jar
 mv server.properties.orig server.properties
+chmod 644 coremods/GuiAPI*
