b10a394b28a1eea20f01fe88c6f912d01381bac2
Agaricus <agaricusb@yahoo.com>
2013-04-20 16:25:46 -0700

    Exclude Rei Minimap from server side

--- go15.sh
+++ go15.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 chmod 000 coremods/GuiAPI*
+chmod 000 mods/*ReiMinimap*
 cp server.properties server.properties.orig
 rm -rf *log*
 #java -Dfml.debugClassLoading=true -Dfml.debugClassLoadingFiner=true -mx2G -jar snapshot.jar
 java -mx2G -jar snapshot.jar
 mv server.properties.orig server.properties
 chmod 644 coremods/GuiAPI*
+chmod 644 mods/*ReiMinimap*
