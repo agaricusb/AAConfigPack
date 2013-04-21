f84d80e79422fa9ad4a2245e3400cf5e3cd01513
Agaricus <agaricusb@yahoo.com>
2013-04-20 17:09:21 -0700

    Use MCPC+ jar patched with Player API

--- go15.sh
+++ go15.sh
@@ -8,6 +8,7 @@ mv mods/*ReiMinimap* clientmods/
 
 # run server
 #java -Dfml.debugClassLoading=true -Dfml.debugClassLoadingFiner=true -mx2G -jar snapshot.jar
-java -mx2G -jar snapshot.jar
+#java -mx2G -jar snapshot.jar
+java -mx2G -jar 354+sm13.jar
 
 . enable-client-mods.sh
