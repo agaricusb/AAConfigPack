5588a5c6229ba92b4830f2f572dd859e53a97f82
Agaricus <agaricusb@yahoo.com>
2013-04-28 13:31:03 -0700

    Increase PermGen size to 256 MB

--- go15.sh
+++ go15.sh
@@ -8,7 +8,7 @@ mv mods/*ReiMinimap* clientmods/
 
 # run server
 #java -Dfml.debugClassLoading=true -Dfml.debugClassLoadingFiner=true -mx2G -jar snapshot.jar
-java -mx2G -jar snapshot.jar
+java -mx2G -XX:MaxPermSize=256m -jar snapshot.jar
 #java -mx2G -jar 354+sm13.jar
 #java -Dorg.spigotmc.netty.disabled=true -mx2G -jar snapshot.jar
 #java -agentpath:/Users/admin/Downloads/YourKit_Java_Profiler_12.0.5.app/bin/mac/libyjpagent.jnilib -mx2G -jar snapshot.jar
