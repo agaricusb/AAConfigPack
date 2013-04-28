448813b9b6b777babe52b9654546ea5dbc4d28e1
Agaricus <agaricusb@yahoo.com>
2013-04-27 19:01:49 -0700

    Applied Energistics: match block IDs to FTB 1.4.7, 2510-2512

--- go15.sh
+++ go15.sh
@@ -10,5 +10,7 @@ mv mods/*ReiMinimap* clientmods/
 #java -Dfml.debugClassLoading=true -Dfml.debugClassLoadingFiner=true -mx2G -jar snapshot.jar
 java -mx2G -jar snapshot.jar
 #java -mx2G -jar 354+sm13.jar
+#java -Dorg.spigotmc.netty.disabled=true -mx2G -jar snapshot.jar
+#java -agentpath:/Users/admin/Downloads/YourKit_Java_Profiler_12.0.5.app/bin/mac/libyjpagent.jnilib -mx2G -jar snapshot.jar
 
 . enable-client-mods.sh
